30450c7fbe646646d2158e946f7ae510f83ef51d
firehelmet <firehelmet@yahoo.com>
2019-11-21 17:43:29 -0800

    parallel heatmaps

---
+++ Heatmap Cost Ranking/static/js/wallethub-weather.js
@@ -0,0 +1,44 @@
+var myMap2 = L.map("map2", {
+  center: [36.8283, -98.5795],
+  zoom: 3
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap2);
+
+d3.json("static/js/city_master_wallethub2.json", function(cityData) {
+
+    for (var i = 0; i < 100; i++) {
+ 
+     var coord = [cityData[i].latitude, cityData[i].longitude]
+    // Color-code by magnitude
+
+    if (cityData[i].weather > 80) {
+        var color = "maroon"
+      } 
+      else if (cityData[i].weather >= 60 && cityData[i].weather <=80) {
+        var color = "red"
+      } 
+      else if (cityData[i].weather >= 40 && cityData[i].weather <=60) {
+        var color = "orange"
+      }
+      else if (cityData[i].weather >= 20 && cityData[i].weather <=40) {
+        var color = "yellow"
+      }
+      else {
+        var color = "green"
+      }
+       /// test section
+       L.circle(coord, {
+         fillOpacity: 0.75,
+         color: "white",
+         fillColor: color,
+         radius: (100000)
+       }).bindPopup("<p> City: " + cityData[i].city + "<p> <p> Weather Rank: " + cityData[i].weather + "</p>").addTo(myMap2);
+    }
+});
